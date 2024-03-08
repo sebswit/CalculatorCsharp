f40ba37449c7521eb8b3bb8330d00f8260cfb507
sebswit <sebastian.switakowski@gmail.com>
2024-03-08 21:51:13 +0000

    added numbers and options

--- Program.cs
+++ Program.cs
@@ -7,6 +7,27 @@ namespace MyApp
         static void Main(string[] args)
         {
 
+            double num1 = 0;
+            double num2 = 0;
+            double result = 0;
+
+
+            Console.WriteLine("------------------");
+            Console.WriteLine("Calculator Program");
+            Console.WriteLine("------------------");
+
+
+            Console.Write("Enter number 1: ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter number 2: ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Pick an option: ");
+            Console.WriteLine("\t+ add");
+            Console.WriteLine("\t- substract");
+            Console.WriteLine("\t* multiply");
+            Console.WriteLine("\t/ divide");
 
 
             Console.ReadKey();
